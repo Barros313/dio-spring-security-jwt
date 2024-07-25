e29e77ba86f4a943c24942fab315ab0e5465b7a2
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-25 10:39:03 -0300

    Latest commit
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/springsecurityjwt/SpringsecurityjwtApplication.java
+++ src/main/java/com/barros/springsecurityjwt/SpringsecurityjwtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringsecurityjwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityjwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityjwtApplication.class, args);
+    }
 
 }
